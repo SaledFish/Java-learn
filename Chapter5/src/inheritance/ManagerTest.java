d496769699c7180bc7b8252c361791aab5689d37
SaledFish <22898907686@qq.com>
2017-04-10 22:02:57 +0800

    Chapter5

---
+++ Chapter5/src/inheritance/ManagerTest.java
@@ -0,0 +1,27 @@
+package inheritance;
+/**
+ * Created by Administrator on 2017/4/10.
+ * This program demonstrates inheritance.
+ */
+public class ManagerTest {
+    public static void main(String[] ares)
+    {
+        //construct a Manager object
+        Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+        boss.setBonus(5000);
+
+        Employee[] staff = new Employee[3];
+
+        //fill the staff array with Mfanager and Employee objects
+        staff[0] = boss;
+        staff[1] = new Employee("Harry Hacker", 5000, 1989, 10, 1);
+        staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
+
+        //print out information about all Employee objects
+        for(Employee e : staff)
+        {
+            System.out.println("name = " + e.getName() + ", salary = "
+            + e.getSalary());
+        }
+    }
+}
