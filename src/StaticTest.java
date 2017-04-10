1f2bb8e353a6302d72539cdf815de78d128a768b
SaledFish <22898907686@qq.com>
2017-04-10 16:23:35 +0800

    static test

---
+++ src/StaticTest.java
@@ -0,0 +1,77 @@
+/**
+ * Created by Administrator on 2017/4/10.
+ * This program demonstrates static methods.
+ */
+public class StaticTest {
+    public static void main(String[] args)
+    {
+        //fill the staff array with three Employee objects
+        Employee[] staff = new Employee[3];
+
+        staff[0] = new Employee("Tom", 40000);
+        staff[1] = new Employee("Dick", 60000);
+        staff[2] = new Employee("Harry", 65000);
+
+        //print out information about all Employee objects
+        for(Employee e : staff)
+        {
+            e.setId();
+            System.out.println("name = " + e.getName() + ", id = " + e.getId() +
+            ", salary = " + e.getSalary());
+
+            int n = Employee.getnextId();//calls static method
+            System.out.println("Next available id = " + n);
+        }
+
+
+
+    }
+}
+
+class Employee
+{
+    private static int nextId = 1;
+
+    private String name;
+    private double salary;
+    private int id;
+
+    public Employee(String n, double s)
+    {
+        name = n;
+        salary = s;
+        id = 0;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId()
+    {
+        id = nextId;//set id to next available field
+        nextId++;
+    }
+
+    public static int getnextId()
+    {
+        return nextId;//returns static field
+    }
+
+    public static void main(String[] args)//unit test
+    {
+        Employee e = new Employee("Harry", 50000);
+        System.out.println(e.getName() + " " + e.getSalary());
+    }
+}
+
